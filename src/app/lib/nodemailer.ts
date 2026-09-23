06e12f10e921c0f61b5763d143a3df8e26c5c794
Tanvir Sorforaz <tanvirsf12345@gmail.com>
2026-09-23 19:47:02 +0600

    fix smtp ipv4 connection

--- src/app/lib/nodemailer.ts
+++ src/app/lib/nodemailer.ts
@@ -1,6 +1,9 @@
 import nodemailer from "nodemailer";
+import dns from "node:dns";
 
 import config from "../config/index.js";
+
+dns.setDefaultResultOrder("ipv4first");
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
